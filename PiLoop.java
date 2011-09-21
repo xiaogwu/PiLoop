9e9c44542fb857e0a630eb116f0df95adab164e1
Xiao G. Wu <xiaogwu@gmail.com>
2011-09-21 11:35:47 -0700

    Initial Commit

---
+++ PiLoop.java
@@ -0,0 +1,24 @@
+/* Xiao G. Wu
+ * PiLoop - Class Exercise
+ * 9/19/2011
+ */
+
+/** This class calculates the value of Pi */
+
+public class PiLoop 
+{
+    public static void main(String[] args)
+    {
+        int d = 3; // Pi expansion initial denominator
+        double pi = 4.0; // Initial estimate for Pi
+        final int iterations = 3000000; // How man times to iterate
+        for (int i = 1; i <= iterations; i++)
+        {
+            System.out.print("Iteration #" + i + "\t"); // Print iteration
+            pi = pi + (4.0 / d)*Math.pow(-1,i); // Use Math.pow to alternate sign of expansion term 
+            System.out.print("pi = " + pi + "\t"); // Current estimate of Pi
+            System.out.printf("Diff: %f\n", Math.abs(Math.PI - pi)); // Absolute difference between estimate and Math.PI
+            d += 2; // Denominator increases by 2 
+        }
+    }
+}
