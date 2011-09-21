51c3a5e434df4868f64cc1bfaf47fe479270be3f
Xiao G. Wu <xiaogwu@gmail.com>
2011-09-21 11:45:26 -0700

    Corrected expansion to Gregory-Leibniz series

--- PiLoop.java
+++ PiLoop.java
@@ -3,7 +3,7 @@
  * 9/19/2011
  */
 
-/** This class calculates the value of Pi */
+/** This class estimates the value of Pi using Gregory-Leibniz series */
 
 public class PiLoop 
 {
@@ -12,10 +12,11 @@ public class PiLoop
         int d = 3; // Pi expansion initial denominator
         double pi = 4.0; // Initial estimate for Pi
         final int iterations = 3000000; // How man times to iterate
+
         for (int i = 1; i <= iterations; i++)
         {
             System.out.print("Iteration #" + i + "\t"); // Print iteration
-            pi = pi + (4.0 / d)*Math.pow(-1,i); // Use Math.pow to alternate sign of expansion term 
+            pi = pi + (4.0 / d)*Math.pow(-1,i); // Use Math.pow to alternate sign of the series 
             System.out.print("pi = " + pi + "\t"); // Current estimate of Pi
             System.out.printf("Diff: %f\n", Math.abs(Math.PI - pi)); // Absolute difference between estimate and Math.PI
             d += 2; // Denominator increases by 2 
